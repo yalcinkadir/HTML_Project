6792d7d599301e9ac33f3cedf0cb4b70ef2b5463
Kadir Yalcin <ygulsun.sahin@gmail.com>
2022-12-26 22:15:46 +0100

    new excircises

---
+++ Exercises/1/js/oop.js
@@ -0,0 +1,17 @@
+
+    let val;
+
+    let person = {
+        name: 'Kadir',
+        geburtsjahr: 1982
+    };
+
+    val = person;
+
+    let numbers = [10,20,30];
+    val = numbers;
+
+
+    console.log(typeof test);
+    console.log(person);
+    console.log("test");
